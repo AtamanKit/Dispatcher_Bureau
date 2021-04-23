f428363529071fa755325c642423f4474f4bfc74
AtamanKit <rodionatamaniuc@gmail.com>
2021-04-23 23:26:43 +0300

    DONE, dates DS/AL, Analiza

--- bir_disp.py
+++ bir_disp.py
@@ -4792,6 +4792,136 @@ class mainWindow(QMainWindow):
                                             )"""
                                          )
                         self.conn.commit()
+
+                        # Determin incadrarea termenului urban, rural
+                        myList = myDeltaHour.split(":")
+                        myMinute = int(myList[0]) * 60 + int(myList[1])
+                        termText = "Incadrat"
+                        compens = 0
+
+                        if myMinute > 8 * 60:
+                            difMinute = myMinute - 8 * 60
+                            termText = "Depasit cu: " + str(round(difMinute / 60)) + "H " + myList[1] + "min."
+                            if difMinute / 60 <= 3:
+                                k = 1
+                            elif (difMinute / 60 > 3) and (difMinute / 60 <= 6):
+                                k = 4
+                            elif (difMinute / 60 > 6) and (difMinute / 60 <= 9):
+                                k = 7
+                            elif (difMinute / 60 > 9):
+                                k = 10
+                            compens = round(0.01 * 160 * 2.04 * k, 2)
+
+                        # Introduc datele in Postgres analiza lunara
+                        self.cur.execute("SELECT * FROM pg_tables "
+                                         "WHERE SCHEMANAME='public'")
+                        isTable = False
+                        for table_tuple in self.cur.fetchall():
+                            if table_tuple[1] == f"decpr_" \
+                                                 f"{self.alYear}_" \
+                                                 f"{self.alMonth}":
+                                isTable = True
+                                self.cur.execute(f"SELECT decpr_id FROM decpr_"
+                                                 f"{self.alYear}_{self.alMonth} "
+                                                 f"ORDER BY decpr_id DESC LIMIT 1")
+
+                                nrDec = self.cur.fetchall()[0][0] + 1
+                                timeNow = self.alTime.strftime("%d.%m.%y %H:%M")
+                                self.cur.execute(f"""INSERT INTO decpr_{self.alYear}_{self.alMonth} (
+                                                        oficiul, 
+                                                        nr_ordine, 
+                                                        pt, 
+                                                        fid_04kv, 
+                                                        data_dec, 
+                                                        data_conect, 
+                                                        durata, 
+                                                        cons_cas, 
+                                                        cons_ec, 
+                                                        total, 
+                                                        localitate, 
+                                                        cauza, 
+                                                        termen, 
+                                                        compens
+                                                ) 
+                                                VALUES (
+                                                        '{self.data.at[self.modRow, 0]}', 
+                                                        '{nrDec}', 
+                                                        '{self.data.at[self.modRow, 4]}', 
+                                                        '{self.data.at[self.modRow, 6]}', 
+                                                        '{valuePregList[1]}', 
+                                                        '{timeNow}', 
+                                                        '{myDeltaHour}', 
+                                                        '{self.fidNrCas}', 
+                                                        '{self.fidNrEc}', 
+                                                        '{self.fidNrCas + self.fidNrEc}', 
+                                                        '{self.data.at[self.modRow, 5]}', 
+                                                        '{self.data.at[self.modRow, 7]}', 
+                                                        '{termText}', 
+                                                        '{compens}')"""
+                                                 )
+                                self.conn.commit()
+                        if isTable == False:
+                            self.cur.execute(f"""CREATE TABLE
+                                                    decpr_{self.alYear}_{self.alMonth}(
+                                                    decpr_id serial PRIMARY KEY,
+                                                    oficiul VARCHAR(32),
+                                                    nr_ordine INT4,
+                                                    pt VARCHAR(32),
+                                                    fid_04kv VARCHAR(32),
+                                                    data_dec VARCHAR(32),
+                                                    data_conect VARCHAR(32),
+                                                    durata VARCHAR(32),
+                                                    cons_cas INT4,
+                                                    cons_ec INT4,
+                                                    total INT4,
+                                                    localitate VARCHAR(32),
+                                                    cauza VARCHAR(256),
+                                                    termen VARCHAR(256),
+                                                    compens NUMERIC
+                                                    )""")
+                            self.conn.commit()
+                            self.cur.execute(f"SELECT decpr_id FROM decpr_"
+                                             f"{self.alYear}_{self.alMonth} "
+                                             f"ORDER BY decpr_id DESC LIMIT 1")
+                            if self.cur.fetchall() == []:
+                                nrDec = 1
+                            else:
+                                nrDec = self.cur.fetchall()[0][0] + 1
+                            timeNow = self.alTime.strftime("%d.%m.%y %H:%M")
+                            self.cur.execute(f"""INSERT INTO decpr_{self.alYear}_{self.alMonth} (
+                                                    oficiul, 
+                                                    nr_ordine, 
+                                                    pt, 
+                                                    fid_04kv, 
+                                                    data_dec, 
+                                                    data_conect, 
+                                                    durata, 
+                                                    cons_cas, 
+                                                    cons_ec, 
+                                                    total, 
+                                                    localitate, 
+                                                    cauza, 
+                                                    termen, 
+                                                    compens
+                                            ) 
+                                            VALUES (
+                                                    '{self.data.at[self.modRow, 0]}', 
+                                                    '{nrDec}', 
+                                                    '{self.data.at[self.modRow, 4]}', 
+                                                    '{self.data.at[self.modRow, 6]}', 
+                                                    '{valuePregList[1]}', 
+                                                    '{timeNow}', 
+                                                    '{myDeltaHour}', 
+                                                    '{self.fidNrCas}', 
+                                                    '{self.fidNrEc}', 
+                                                    '{self.fidNrCas + self.fidNrEc}', 
+                                                    '{self.data.at[self.modRow, 5]}', 
+                                                    '{self.data.at[self.modRow, 7]}', 
+                                                    '{termText}', 
+                                                    '{compens}')"""
+                                             )
+                            self.conn.commit()
+
                         self.cur.close()
 
                         # #Introduc datele in Excel analiza anuala
